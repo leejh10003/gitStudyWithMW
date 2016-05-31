acc7ac4db15803f1df8e2bb345dbe3c6a0e3dc1c
JunHyuk <JunHyuk@JOONJI-LAPTOP>
2016-05-31 18:43:18 +0900

    macro function added :)

--- HelloWorld/HelloWorld/Source.c
+++ HelloWorld/HelloWorld/Source.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <GL\GL.h>
+#define ADD(a,b) ((a)+(b))
 
 int main()
 {
+	int a = 10;
+	int b = 40;
+	int c = ADD(1, b);
 	printf("Hello, Wrodld!\n");
+	printf("%d", c);
 	system("pause");
 	return 0;
 }
